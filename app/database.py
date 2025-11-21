304cb7e657ff47497805514e860c2f5cba89ad6d
snarz <ingeariza@live.com>
2025-11-21 11:28:46 -0500

    correcciones para render

--- app/database.py
+++ app/database.py
@@ -1,23 +1,15 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DB_USER = os.getenv('DB_USER', 'rental_user')
-DB_PASS = os.getenv('DB_PASS', 'rental_pass')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_PORT = os.getenv('DB_PORT', '3306')
-DB_NAME = os.getenv('DB_NAME', 'rental_services')
+# Render requiere sslmode=require
+if DATABASE_URL and "sslmode" not in DATABASE_URL:
+    DATABASE_URL += "?sslmode=require"
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
-
-engine = create_engine(DATABASE_URL, echo=False, future=True)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
-def init_db():
-    # Import models to ensure they are registered with SQLAlchemy metadata
-    import app.models as models  # noqa
-    Base.metadata.create_all(bind=engine)
+Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -3,6 +3,47 @@ from app.database import init_db
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import services, technicians, agendas, users
 from .db_init import init_database
+from app.database import Base, engine
+from database import Base, engine
+import models
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import User
+import uuid
+
+def create_default_admin():
+    db: Session = SessionLocal()
+    admin_email = os.getenv("ADMIN_EMAIL")
+    admin_password = os.getenv("ADMIN_PASSWORD")
+
+    if not admin_email or not admin_password:
+        print("ADMIN_EMAIL o ADMIN_PASSWORD no configurados.")
+        return
+
+    admin = db.query(User).filter(User.email == admin_email).first()
+
+    if not admin:
+        print("Creando usuario admin por defecto...")
+        new_admin = User(
+            id=str(uuid.uuid4()),
+            name="Admin",
+            email=admin_email,
+            password=admin_password,
+            role="admin"
+        )
+        db.add(new_admin)
+        db.commit()
+    else:
+        print("Admin ya existe.")
+
+    db.close()
+
+
+# LLAMAR AL INICIO
+create_default_admin()
+
+print("Creando tablas si no existen...")
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='rental-services-python', redirect_slashes=False)
 app.add_middleware(
