4165a3befb24ca28b683e29d392b9775682f7e46
snarz <ingeariza@live.com>
2025-11-28 23:21:17 -0500

    crud create_agenda contain as .op

--- app/crud.py
+++ app/crud.py
@@ -112,7 +112,7 @@ def create_agenda(db: Session, agenda: schemas.AgendaCreate):
 
     # 1. Obtener técnicos cuya especialidad coincida
     technicians = db.query(models.Technician).filter(
-        models.Technician.specialties.contains([agenda.service])
+        models.Technician.specialties.op("@>")([agenda.service])
     ).all()
 
     if not technicians:
